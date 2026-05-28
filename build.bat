9dd4febcea70d5da220ef9bfb18913eaa517c7d2
Tikhon <tikhon.shmerkin@mail.ru>
2026-05-28 18:17:57 +0300

    v0.01

---
+++ build.bat
@@ -0,0 +1,2 @@
+@echo off
+g++ -std=c++20 -O2 -Wall -Wextra main.cpp -o sort_lab.exe
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "sort-lab variant 21: ok\n";
+    return 0;
+}
